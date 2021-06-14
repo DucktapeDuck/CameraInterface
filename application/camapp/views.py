5cfeb032c3dda7a01fdea680933b95553cdd75a1
Connor <c.frost.819@gmail.com>
2021-06-13 22:20:32 -0600

    :sparkles: Webcam working

--- application/camapp/views.py
+++ application/camapp/views.py
@@ -19,7 +19,7 @@ class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
         (self.grabbed, self.frame) = self.video.read()
-        # threading.Thread(target=self.update, args=()).start()
+        threading.Thread(target=self.update, args=()).start()
 
     def __del__(self):
         self.video.release()
